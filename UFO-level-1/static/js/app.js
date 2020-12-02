0410972147e4252e1e7375e9bdf6d439e98864d8
Travis Molnoskey <t.molnoskey@gmail.com>
2020-12-01 16:06:28 -0800

    table data loaded, filter in progress

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,25 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+let tbody = d3.select("tbody");
+
+tableData.forEach((item) => {
+    var row = tbody.append("tr");
+    Object.entries(item).forEach(([key, value]) => {
+      var entry = row.append("td");
+      entry.text(value);
+    });
+  });
+
+let filterButton = d3.select("#filter-btn")
+
+let inputForm = d3.select("#datetime")
+
+filterButton.on("click",filterData);
+
+function filterData(){
+    let inputValue =  inputForm.property("value")
+    let filtered = tableData.filter(entries=>entries.datetime === inputValue)
+    filtered
+
+}
