2787df70f80479ac1c5db8e7ed2778726433bc8c
Travis Molnoskey <t.molnoskey@gmail.com>
2020-12-01 16:38:07 -0800

    finished date filter function

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -10,16 +10,30 @@ tableData.forEach((item) => {
       entry.text(value);
     });
   });
-
+//assign variable for button element
 let filterButton = d3.select("#filter-btn")
-
+//assign variable for form element
 let inputForm = d3.select("#datetime")
 
-filterButton.on("click",filterData);
-
+//function to filter data
 function filterData(){
-    let inputValue =  inputForm.property("value")
-    let filtered = tableData.filter(entries=>entries.datetime === inputValue)
-    filtered
+    d3.event.preventDefault();
+    //grab filter form input value
+    let inputValue =  inputForm.property("value");
+    //filter data on input value
+    let filteredData = tableData.filter(entries=>entries.datetime === inputValue);
+    console.log(filteredData);
+    //remove current body of table
+    tbody.html("");
+    //fill table with filtered data
+    filteredData.forEach((filterItem) => {
+        var row = tbody.append("tr");
+        Object.entries(filterItem).forEach(([key, value]) => {
+          var entry = row.append("td");
+          entry.text(value);
+        });
+      });
+};
 
-}
+//pass function on button click
+filterButton.on("click",filterData);
